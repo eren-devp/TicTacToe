ea289a73546261bcc861ee5676155ad4c3249cdd
eren-devp <talha.genc@bahcesehir.edu.tr>
2023-12-24 00:59:59 +0300

    Drawing the map

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ import numpy as np
 
 def get_input(user : str, game_map):
     while True:
-        print(game_map)
+        print_map(game_map)
 
         play = int(input(f"Player {user} please make your move (1-9): ")) - 1
         if play < 0 or play >= 9:
             print("Please enter a valid input!")
             continue
 
-        if not game_map[int(play / 3)][play % 3] == "":
+        if not game_map[int(play / 3)][play % 3] == " ":
             print("The specified square is not empty!")
             continue
 
         game_map[int(play / 3)][play % 3] = user
         break
+
+def print_map(game_map):
+    print()
+    for i in game_map:
+        print("---------------")
+        for j in i:
+            print(f"| {j} |", end="")
+        print()
+    print("---------------")
 
 def winning(game_map):
     winner = " "
@@ -33,6 +42,7 @@ def winning(game_map):
         winner = "O"
 
     if not winner == " ":
+        print_map(game_map)
         print(f"{winner} won the game!")
 
     return winner == " "
@@ -40,13 +50,13 @@ def winning(game_map):
 def draw(game_map):
     for i in game_map:
         for j in i:
-            if j == "":
+            if j == " ":
                 return False
 
     return True
 
 def main():
-    game_map = np.empty((3, 3), str)
+    game_map = np.full((3, 3), " ", dtype="str")
     turn = "O"
 
     while (winning(game_map) and not draw(game_map)):
